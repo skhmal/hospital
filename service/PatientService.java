feb478bbb68b21089a991b2967990f777ca6a965
skhmal <92174133+skhmal@users.noreply.github.com>
2022-07-05 13:38:08 +0200

    Add models, data access.

--- service/PatientService.java
+++ service/PatientService.java
@@ -1,7 +1,8 @@
-package com.khmal.Hosp.service;
+package com.khmal.hospital.service;
 
 import com.khmal.Hosp.entity.Patient;
 
 public interface PatientService {
+
     void savePatient(Patient patient);
 }
